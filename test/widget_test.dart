eba939df82be2b384ca273a1349b691844ed4b33
MaverickB1 <maverickbellee@gmail.com>
2021-08-12 10:50:59 +0800

    second commit

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,8 +7,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:ntu_wayfinder_app/main.dart';
+import 'package:ntu_wayfinder_app/app.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
