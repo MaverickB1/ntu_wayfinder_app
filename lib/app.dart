85d912f8e144ce7adbf118fa7f28f0dec8b1b9c1
MaverickB1 <maverickbellee@gmail.com>
2021-08-12 17:51:31 +0800

    Add Flutter Icons

--- lib/app.dart
+++ lib/app.dart
@@ -1,7 +1,13 @@
+import 'dart:ui';
+
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 class App extends StatelessWidget {
   // This widget is the root of the application.
+
+  final Future<FirebaseApp> _firebaseApp = Firebase.initializeApp();
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -9,8 +15,69 @@ class App extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: Scaffold(
-        body: Container(),
+      home: FutureBuilder(
+        future: _firebaseApp,
+        builder: (context, snapshot) {
+          if (snapshot.hasError) {
+            return const Center(
+              child: Text('Error!'),
+            );
+          } else if (snapshot.hasData) {
+            return HomeScreen();
+          } else {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+        },
+      ),
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    //query size (height & width) of device
+    final size = MediaQuery.of(context).size;
+    return Scaffold(
+      body: Stack(
+        children: <Widget>[
+          Container(
+            //Container size is made to be 45% of the total device height
+            height: size.height * 0.45,
+            decoration: const BoxDecoration(
+              color: Color(0xFFF5CEB8),
+              image: DecorationImage(
+                alignment: Alignment.centerLeft,
+                image:
+                    AssetImage('assets/images/instantsupport_background.png'),
+              ),
+            ),
+          ),
+          SafeArea(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                // ignore: prefer_const_literals_to_create_immutables
+                children: <Widget>[
+                  const Padding(
+                    padding: EdgeInsets.all(16),
+                    child: Text(
+                      'Good Day Lost Adventurer! Let us help you find your way!',
+                      style: TextStyle(
+                        fontSize: 32,
+                        fontFamily: 'Cairo',
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
